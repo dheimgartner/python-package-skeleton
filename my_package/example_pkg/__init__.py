b88b40e0fae1c6f854c9eb6f720c47ade68cf40b
d_heim <d.heimgartners@gmail.com>
2021-01-25 23:37:59 +0100

    Added import in __init__.py

---
+++ my_package/example_pkg/__init__.py
@@ -0,0 +1,5 @@
+# If I do not include this then my_fun has to be imported explicitly:
+# import example_pkg will now allow you to write example_pkg.my_fun...
+# Generally you might not want to force users to import modules when loading the package.
+# But in this case, I want!
+import my_fun
