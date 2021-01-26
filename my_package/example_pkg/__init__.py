3c57a81bfaac62e99da8d613844148e0a3e3214e
d_heim <d.heimgartners@gmail.com>
2021-01-26 22:41:15 +0100

    __init__.py from . import my_fun

--- my_package/example_pkg/__init__.py
+++ my_package/example_pkg/__init__.py
@@ -2,4 +2,4 @@
 # import example_pkg will now allow you to write example_pkg.my_fun...
 # Generally you might not want to force users to import modules when loading the package.
 # But in this case, I want!
-from example_pkg import my_fun
+from . import my_fun
